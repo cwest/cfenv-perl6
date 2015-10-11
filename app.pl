e0867f2767531367204ec3b6877c6d7d0ce315a5
Casey West <casey@geeknest.com>
2015-10-11 09:47:01 -0400

    Initial commit.

---
+++ app.pl
@@ -0,0 +1,15 @@
+use v6;
+use Bailador;
+use JSON::Fast;
+
+my $port = Int(%*ENV<PORT> || 5000);
+
+get '/' => sub {
+	content_type 'application/json';
+
+    my $instance_id = %*ENV<CF_INSTANCE_INDEX> || 'none';
+
+    return to-json({instance => $instance_id});
+}
+
+baile( $port );
